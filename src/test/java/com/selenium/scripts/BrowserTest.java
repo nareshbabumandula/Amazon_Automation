80e86f2071d040824624434c2f0bfa0e74cc1f74
[nareshbabumandula] <naresh223@gmail.com>
2025-06-02 20:37:55 +0530

    Increased the wait time

--- src/test/java/com/selenium/scripts/BrowserTest.java
+++ src/test/java/com/selenium/scripts/BrowserTest.java
@@ -10,7 +10,7 @@ public class BrowserTest {
 		driver = new ChromeDriver();
 		driver.get("https://www.amazon.in");
 		driver.manage().window().maximize();
-		Thread.sleep(3000);
+		Thread.sleep(4000);
 		driver.quit();
 	}
 
